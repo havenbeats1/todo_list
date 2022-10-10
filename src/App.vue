12b5b2e642d40c32db9c505fade16be98af3e7de
haven rutter <108587591+havenbeats1@users.noreply.github.com>
2022-10-09 21:18:38 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,28 @@ const input_content = ref('')
 const input_category = ref(null)
 
 const todos_asc = computed(() => todos.value.sort((a,b) => {
-  return a.createdAt - b.createdAt
+  return b.createdAt - a.createdAt
 }))
 
-const addTodo = () => {}
+const addTodo = () => {
+  if (input_content.value.trim() === '' || input_category.value === null) {
+    return
+  }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  })
+}
+
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
+}
+
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal));
+}, { deep: true })
 
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal)
@@ -19,6 +37,7 @@ watch(name, (newVal) => {
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos') || '[]')
 })
 </script>
 
@@ -44,6 +63,7 @@ onMounted(() => {
         <h4>Pick a category</h4>
 
         <div class="options">
+
           <label>
             <input 
               type="radio" 
@@ -52,10 +72,45 @@ onMounted(() => {
               v-model="input_category" />
               <span class="bubble business"></span>
               <div>Business</div>
-            </label>
+          </label>
+          <label>
+            <input 
+              type="radio" 
+              name="category"
+              value="personal"
+              v-model="input_category" />
+              <span class="bubble personal"></span>
+              <div>Personal</div>
+          </label>
+          
             
         </div>
+
+        <input type="submit" value="Add todo" />
       </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+
+          <label for="">
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+
+        </div>
+
+
+      </div>
     </section>
 
   </main>
